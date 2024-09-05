df361eaf7a1477165403af0653a1937d27bbebd4
Saulo Candeira <saulocandeira@hotmail.com>
2024-09-05 14:46:26 -0300

    Add: primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/como-criar-um-web-site-v2/"
+  base: "/como-criar-um-website-v2/"
 })
