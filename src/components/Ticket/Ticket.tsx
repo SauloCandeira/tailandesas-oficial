d71c508fc737e90254fc2bb29523067a8323e4c2
Saulo Candeira <saulocandeira@hotmail.com>
2024-09-06 16:45:05 -0300

    Add: adicionado menu

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './Ticket.css'; // Certifique-se de que seus estilos CSS estejam no arquivo Ticket.css
-import { Product } from '../../Interfaces/InterfaceProduct';
+import eBookImg from './../../assets/img/e-book.png';
+import videoImg from './../../assets/img/video.png';
+import packageImg from './../../assets/img/package.png'; // Certifique-se de ajustar os caminhos das imagens
 
-interface TicketProps {
-  products: Product[];
-}
+const Ticket: React.FC = () => {
+  const { t, i18n } = useTranslation();
 
-const Ticket: React.FC<TicketProps> = ({ products }) => {
-  const { t } = useTranslation();
+  // Definindo os produtos diretamente dentro do componente
+  const products = [
+    {
+      id: 1,
+      name: 'E-book',
+      price: t('ticket.eBookPrice'), // Traduzindo o preço
+      description: t('ticket.eBookDescription'), // Traduzindo a descrição
+      image: eBookImg,
+      link: 'https://wa.me/5561981374279?text=Gostaria%20de%20reservar%20o%20pacote%20E-book',
+    },
+    {
+      id: 2,
+      name: 'Videoaula',
+      price: t('ticket.videoPrice'), // Traduzindo o preço
+      description: t('ticket.videoDescription'), // Traduzindo a descrição
+      image: videoImg,
+      link: 'https://wa.me/5561981374279?text=Gostaria%20de%20reservar%20o%20pacote%20Videoaula',
+    },
+    {
+      id: 3,
+      name: 'Pacote Completo',
+      price: t('ticket.completePackagePrice'), // Traduzindo o preço
+      description: t('ticket.completePackageDescription'), // Traduzindo a descrição
+      image: packageImg,
+      link: 'https://wa.me/5561981374279?text=Gostaria%20de%20reservar%20o%20pacote%20Completo',
+    },
+  ];
+
+  const getCurrencySymbol = () => {
+    return i18n.language === 'pt' ? 'R$' : '$'; // Se for português, usa Real, senão, Dólar
+  };
 
   return (
     <div id="embarque" className="ticket-section">
@@ -19,14 +49,16 @@ const Ticket: React.FC<TicketProps> = ({ products }) => {
 
       <div className="container">
         <div className="card-content">
-          {products.map((product: Product) => (
+          {products.map((product) => (
             <div key={product.id} className="package">
               <img src={product.image} alt={product.name} className="w-32 mx-auto mb-4" />
               <h2 className="text-3xl font-semibold mb-2">{product.name}</h2>
               <p className="text-lg mb-4">{product.description}</p>
-              <p className="text-2xl font-bold mb-6">{product.price}</p>
-              <a href={product.link} className="reserve-button">
-                {t('ticket.reserve')}
+              <p className="text-2xl font-bold mb-6">
+                {getCurrencySymbol()} {Number(product.price).toFixed(2)}
+              </p>
+              <a href={product.link} className="reserve-button" target="_blank" rel="noopener noreferrer">
+                {t('ticket.reserveNow')}
               </a>
             </div>
           ))}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,39 +12,14 @@ import { Product } from '../../Interfaces/InterfaceProduct';
 
 const Home = () => {
 
-  const products: Product[] = [
-    {
-      id: 1,
-      name: 'E-book', // Adicione a propriedade name
-      price: 49.99, // Ajuste o tipo para number
-      description: 'Aprenda com nosso e-book detalhado, cobrindo todos os aspectos essenciais.',
-      image: eBookImg, // Adicione a propriedade image
-      link: 'https://wa.me/5561981374279?text=Gostaria%20de%20reservar%20o%20pacote%20E-book',
-    },
-    {
-      id: 2,
-      name: 'Videoaula', // Adicione a propriedade name
-      price: 79.99, // Ajuste o tipo para number
-      description: 'Assista nossa videoaula e pratique com exemplos práticos.',
-      image: videoImg, // Adicione a propriedade image
-      link: 'https://wa.me/5561981374279?text=Gostaria%20de%20reservar%20o%20pacote%20Videoaula',
-    },
-    {
-      id: 3,
-      name: 'Pacote Completo', // Adicione a propriedade name
-      price: 119.99, // Ajuste o tipo para number
-      description: 'Obtenha o e-book e as videoaulas juntos com um preço especial.',
-      image: packageImg, // Adicione a propriedade image
-      link: 'https://wa.me/5561981374279?text=Gostaria%20de%20reservar%20o%20pacote%20Completo',
-    },
-  ];
+
 
   return (
     <div>
       <HeaderTwo/>
       <Intro />
       <Timeline />
-      <Ticket products={products} />
+      <Ticket  />
     </div>
   );
 };
