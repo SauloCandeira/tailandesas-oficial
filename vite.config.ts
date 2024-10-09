f6f310177227e6bd6a3e5c5af94d780b1a49c7d0
Saulo Candeira <saulocandeira@hotmail.com>
2024-10-09 15:01:54 -0300

    Add: adicionando

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/tailandesas-oficial/"
-  base: "https://tailandesasoficial.com.br/"
+  base: "/tailandesas-oficial/"
+  // base: "https://tailandesasoficial.com.br/"
 })
