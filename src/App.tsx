0cf7a7d570c957c37c1f0600d2eec59ff4576409
Saulo Candeira <saulocandeira@hotmail.com>
2024-10-09 15:11:32 -0300

    Add: adicionando

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import MarketPlace from './pages/MarketPlace/MarketPlace';
 import './i18n'; // Importa o arquivo de configuração do i18next
 import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Home from './pages/Home/Home';
+// import Home from './pages/Home/Home';
 
 export function App() {
   return (
     
     <BrowserRouter>
       <Routes>
-        <Route path="/tailandesas-oficial/" element={<MarketPlace />} />
+        <Route path="/" element={<MarketPlace />} />
         <Route path="/tailandesas-oficial/login" element={<Login />} />
         <Route path="/tailandesas-oficial/dashboard" element={<Dashboard /> }/>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/project/:id" element={<ProjectDetails />} /> */}
       </Routes>
     </BrowserRouter>
